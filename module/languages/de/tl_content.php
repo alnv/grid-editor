108a23c0efb7d56d9cfa5a23e9bfec759a465225
David Molineus <mail@netzmacht.de>
2014-07-17 12:50:56 +0200

    remove wrong copyright statement

--- module/languages/de/tl_content.php
+++ module/languages/de/tl_content.php
@@ -1,10 +1,6 @@
 <?php
 
 /**
- * Contao Open Source CMS
- *
- * Copyright (C) 2005-2012 Leo Feyer
- *
  * @package   netzmacht-columnset
  * @author    David Molineus <http://www.netzmacht.de>
  * @license   GNU/LGPL
